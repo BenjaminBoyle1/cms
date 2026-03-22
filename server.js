129f89ef1cd75e11ee27ba37d93559678fb48aab
BenjaminBoyle1 <boyle153@gmail.com>
2026-03-21 18:27:20 -0600

    mongodb connection

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 // import the routing file to handle the default (index) route
 var index = require('./server/routes/app');
 
@@ -14,6 +14,13 @@ var documentRoutes = require('./server/routes/documents');
 var messageRoutes = require('./server/routes/messages');
 var contactRoutes = require('./server/routes/contacts');
 
+mongoose.connect('mongodb://localhost:27017/cms')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch((err) => {
+    console.log('Connection failed: ' + err);
+  });
 var app = express(); // create an instance of express
 
 // Tell express to use the following parsers for POST data
--- server/routes/documents.js
+++ server/routes/documents.js
@@ -1,4 +1,78 @@
 var express = require('express');
 var router = express.Router();
 
+const sequenceGenerator = require('./sequenceGenerator');
+const Document = require('../models/document');
+
+router.get('/', (req, res) => {
+  Document.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Documents fetched successfully!',
+        documents: documents
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'An error occurred',
+        error: error
+      });
+    });
+});
+
+router.post('/', (req, res) => {
+  const maxDocumentId = sequenceGenerator.nextId("documents");
+
+  const document = new Document({
+    id: maxDocumentId,
+    name: req.body.name,
+    description: req.body.description,
+    url: req.body.url
+  });
+
+  document.save()
+    .then(createdDocument => {
+      res.status(201).json({
+        message: 'Document added successfully',
+        document: createdDocument
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'An error occurred',
+        error: error
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Document.findOne({ id: req.params.id })
+    .then(document => {
+      document.name = req.body.name;
+      document.description = req.body.description;
+      document.url = req.body.url;
+
+      Document.updateOne({ id: req.params.id }, document)
+        .then(() => {
+          res.status(204).json({
+            message: 'Document updated successfully'
+          });
+        });
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: 'Document not found.'
+      });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Document.deleteOne({ id: req.params.id })
+    .then(() => {
+      res.status(204).json({
+        message: 'Deleted'
+      });
+    });
+});
+
 module.exports = router;
